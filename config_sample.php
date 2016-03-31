eee155d21f5832cb41fa0a50e02ccf4473b36d7a
mattvanzanten@gmail.com <Matt@archer.local>
2016-03-31 12:23:00 -0700

    clean up

--- config_sample.php
+++ config_sample.php
@@ -1,10 +1,6 @@
 <?php
-//connect to db
-error_reporting(-1);
-ini_set('display_errors', 'On');
-$dsn 		= 'mysql:dbname=;host=localhost';
-$user 		= '';
-$password 	= '';
-$connect 	= new PDO($dsn, $user, $password, array(\PDO::MYSQL_ATTR_INIT_COMMAND =>  'SET NAMES utf8') );
+//error_reporting(-1);
+//ini_set('display_errors', 'On');
 date_default_timezone_set('America/Vancouver');
 $to = 'email_address';
+$url = '';
